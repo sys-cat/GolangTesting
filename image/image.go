18dfca204abbc4e927feed46d0aeafffac8918a3
sys-cat <systemcat91@gmail.com>
2017-04-28 01:03:39 +0900

    checked image file size

--- image/image.go
+++ image/image.go
@@ -18,6 +18,36 @@ func Run() (os.FileInfo, error) {
 		return nil, err
 	}
 	fmt.Println(file.Size())
+	f, e := os.Open("doc.png")
+	if e != nil {
+		return nil, err
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	g := gift.New()
+	dst := image.NewRGBA(g.Bounds(img.Bounds()))
+	g.Draw(dst, img)
+	n, err := os.Create("test.png")
+	if err != nil {
+		return nil, err
+	}
+	err = png.Encode(n, dst)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	nfile, err := os.Stat("test.png")
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(nfile.Size())
+	if nerr := os.Remove("test.png"); err != nil {
+		return nil, nerr
+	}
+
 	return file, nil
 }
 
@@ -31,12 +61,12 @@ func RunOverHttp() bool {
 	fmt.Println(img.ContentLength)
 	body := img.Body
 	img_1, ext, err := image.Decode(body)
-	if err != nil || ext == nil {
+	if err != nil || ext == "" {
 		return false
 	}
 	g := gift.New()
 	dst := image.NewRGBA(g.Bounds(img_1.Bounds()))
-	g.Draw(g, img_1)
+	g.Draw(dst, img_1)
 	b := new(bytes.Buffer)
 	err = png.Encode(b, dst)
 	if err != nil {
